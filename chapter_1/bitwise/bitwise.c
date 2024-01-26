59a6a9c56086cca6bf9d58a212461e6250621eff
FarukEfe <efefrk30@gmail.com>
2024-01-26 00:38:55 -0500

    Started Implementing Bitwise Operations (Needs Debug in Highlighted Part)

--- chapter_1/bitwise/bitwise.c
+++ chapter_1/bitwise/bitwise.c
@@ -3,8 +3,13 @@
 #include <math.h>
 #include <string.h> 
 
+// Function declarations
 int to_binary();
 int to_decimal();
+int logical_and();
+int logical_or();
+int logical_xor();
+
 
 int main() {
 
@@ -25,16 +30,12 @@ int main() {
     printf("%ld\n",bitwise1);
     printf("%ld\n",bitwise2);
     // Get bitwise and/or/xor values
-    /*
-    long int bit_and;
-    long int bit_or;
-    long int bit_xor;
+    
+    long int bit_and = logical_and(bitwise1,bitwise2);
+    long int bit_or = 0;
+    long int bit_xor = 0;
 
-    long int decimal_step = 0;
-
-    while (pow(10,decimal_step) < bitwise1) {
-
-    }*/
+    printf("%ld\n",bit_and);
     // Turn them back in numbers
     printf("%d\n",to_decimal(bitwise1));
     printf("%d\n",to_decimal(bitwise2));
@@ -54,10 +55,10 @@ int to_binary(int n) {
     return binary_num;
 }
 
-int to_decimal(int binary) {
-    long int decimal_step = 0;
+int to_decimal(long int binary) {
+    int decimal_step = 0;
     int total = 0;
-    int step_tenth, rem;
+    int rem;
 
     while (binary > 0) {
         rem = binary % 10;
@@ -66,4 +67,42 @@ int to_decimal(int binary) {
         decimal_step++;
     }
     return total;
+}
+
+int logical_and(int b1, int b2) {
+
+    int larger = b1, smaller = b2;
+    if (larger < smaller) {
+        larger, smaller = smaller, larger;
+    }
+
+    int decimal_step = 0; // Current step in the decimal
+    int r1, r2; // Remainder of both binaries
+    bool p1, p2; // Boolean translations of remainders
+    int dec_val; // Propositional integer value of p1 and p2
+    long int total = 0;
+
+    printf("Large: %d Small: %d\n",larger,smaller);
+    while (larger > 0) {
+        // Find decimal place
+        r1, r2 = larger % 10, smaller % 10;
+        // Convert to boolean type
+        p1, p2 = r1, r2;
+        dec_val = (p1 && p2);
+        printf("Current Decimal Value for %d and %d: %d\n",p1,p2,dec_val);
+        total += dec_val * pow(10,decimal_step);
+        //larger -= r1,smaller -= r2;
+        larger /= 10, smaller /= 10;
+        printf("Large: %d Small: %d\n",larger,smaller);
+        decimal_step++;
+    }
+    return total;
+}
+
+int logical_or(int b1, int b2) {
+
+}
+
+int logical_xor(int b1, int b2) {
+
 }
