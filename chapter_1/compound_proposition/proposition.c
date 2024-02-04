278dcd0a9e29e871b29168b0a1888ae0613f7cb1
FarukEfe <efefrk30@gmail.com>
2024-02-04 15:37:52 -0500

    Installed SWI-Prolog library and compiler. Made replace_char method.

--- chapter_1/compound_proposition/proposition.c
+++ chapter_1/compound_proposition/proposition.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <SWI-Prolog.h> // SWI Prolog c-library
+// Compile this program using swipl-ld -o output proposition.c
+
+void replace_char(char *string, char replace, char new);
 
 int main()
 {   
@@ -26,24 +30,35 @@ int main()
     // Booleans
     bool p;
     bool q;
-    // Use temporary integers to store bool values
-    int temp1;
-    int temp2;
     // Get boolean input
     printf("Enter 0(False) or 1(True) for p: ");
-    scanf("%d", &temp1);
+    scanf("%d", &p);
     printf("\nEnter 0(False) or 1(True) for q: ");
-    scanf("%d", &temp2);
+    scanf("%d", &q);
     // Get script and turn to prolog
     printf("Enter the script. You can use the following words:\n");
     printf("not p\np and q\np or q\np implies q\np equivalent q\n");
     printf("Make sure to nest your statement accordingly to avoid syntax errors (max 100 characters)\n");
     scanf(" %[^\n]", logic);
-    // Call system to execute commands
-    system("swipl -s db.pl");
-    system(logic);
-
-    // Use PWI_Prolog calls to evaluate term using PL_call(term_t,module_t);
+    // Use SWI_Prolog calls to evaluate term using PL_call(term_t,module_t);
     // First check exceptions with PL_exception(term_t);
+    // Pass in term and database module to evaluate query
+    replace_char(logic, 'p', p ? 't' : 'f');
+    replace_char(logic, 'q', q ? 't' : 'f');
+    bool result = PL_call(logic,fptr);
+    if (result) {
+        printf("%s: TRUE");
+    } else {
+        printf("%s: FALSE");
+    }
 
+}
+
+void replace_char(char string[100], char replace, char new) {
+    int length = strlen(string);
+    for (int i=0;i<length;i++) {
+        if (string[i] == replace) {
+            string[i] = new;
+        }
+    }
 }
